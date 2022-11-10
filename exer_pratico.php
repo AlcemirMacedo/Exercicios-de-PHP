428da76a2425048be0a38995096603ee2977fcd8
Alcemir Macedo <alcemirmacedo@gmail.com>
2022-11-10 16:22:57 -0400

    Exercício de PHP

---
+++ exer_pratico.php
@@ -0,0 +1,39 @@
+<?php
+// IDENTIFIQUE O ERRO
+
+// código errado
+/*$lista = [
+    'nome' => 'Alcemir',
+    'idade' => 40,
+    'atributos' => [
+        'forca' => 80,
+        'inteligência' => 100
+    ],
+    vida = 1000,
+    mana = 50
+];
+
+echo "Nome: ".$lista['nome']."<br/>";
+echo "Força: ".$lista['atributos']['forca']."<br/>";
+echo "Vida: ". $lista['vida'];
+
+?>*/
+
+
+
+$lista = [
+    'nome' => 'Alcemir',
+    'idade' => 40,
+    'atributos' => [
+        'forca' => 80,
+        'inteligência' => 100
+    ],
+    'vida' => 1000,
+    'mana' => 50
+];
+
+echo "Nome: ".$lista['nome']."<br/>";
+echo "Força: ".$lista['atributos']['forca']."<br/>";
+echo "Vida: ". $lista['vida'];
+
+?>
