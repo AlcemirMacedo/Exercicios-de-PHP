b0d40be315942b9e47bec7fe93f31920d55b550f
Alcemir Macedo <alcemirmacedo@gmail.com>
2022-11-10 17:19:22 -0400

    Condiais ternário

--- loops_e_condicionais.php
+++ loops_e_condicionais.php
@@ -1,12 +1,18 @@
 <?php
 
 //CONDICIONAL IF
-$idade = 18;
+$idade = 17;
 
 if ($idade < 18){
     echo "Menor de idade";
 }else{
     echo "Maior de idade";
 }
+
+//OPERADOR TERNÁRIO
+//(Copndição) ? RESULTADO POSITIVO : RESULTADO NEGATIVO;
+
+echo "<br/>";
+echo ($idade > 18) ? "Maior de idade" : "Menor de idade";
 
 ?>
